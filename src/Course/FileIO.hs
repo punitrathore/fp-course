ec08e672a9ff545d0a68cddf7c6a06156c8ef063
Punit Rathore <punitr@gmail.com>
2018-05-15 16:46:21 -0700

    finished fileIO

--- src/Course/FileIO.hs
+++ src/Course/FileIO.hs
@@ -85,46 +85,60 @@ printFile ::
   FilePath
   -> Chars
   -> IO ()
-printFile =
-  error "todo: Course.FileIO#printFile"
+printFile fp c = do
+  putStrLn ("============" ++ fp)
+  putStrLn c
+  
 
 -- Given a list of (file name and file contents), print each.
 -- Use @printFile@.
 printFiles ::
   List (FilePath, Chars)
   -> IO ()
-printFiles =
-  error "todo: Course.FileIO#printFiles"
+printFiles l = do
+  sequence $ (<$>) (\(fp, ch) -> printFile fp ch) l
+  return ()
+  
 
 -- Given a file name, return (file name and file contents).
 -- Use @readFile@.
 getFile ::
   FilePath
   -> IO (FilePath, Chars)
-getFile =
-  error "todo: Course.FileIO#getFile"
+getFile fp = do
+  ch <- readFile fp
+  return (fp, ch)
+  
 
 -- Given a list of file names, return list of (file name and file contents).
 -- Use @getFile@.
 getFiles ::
   List FilePath
   -> IO (List (FilePath, Chars))
-getFiles =
-  error "todo: Course.FileIO#getFiles"
+getFiles lst = sequence $ (<$>) getFile lst
+  
 
 -- Given a file name, read it and for each line in that file, read and print contents of each.
 -- Use @getFiles@ and @printFiles@.
 run ::
   FilePath
   -> IO ()
-run =
-  error "todo: Course.FileIO#run"
+run fp = do
+  (_, ch) <- getFile fp
+  let fileNames = lines ch
+  files <- getFiles fileNames
+  printFiles files
+  return ()
+  
 
 -- /Tip:/ use @getArgs@ and @run@
 main ::
   IO ()
-main =
-  error "todo: Course.FileIO#main"
+main = do
+  fps <- getArgs
+  case fps of
+    Nil -> return ()
+    (fp :. _) -> run fp
 
 ----
 
